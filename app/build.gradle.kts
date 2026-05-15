a18d39d6d5a4bf215a7d8aa4156cc3bd3bb60d4e
DINESH K <unknown1603kj@gmail.com>
2026-05-15 15:24:06 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -73,6 +73,8 @@ dependencies {
     implementation(
         "androidx.lifecycle:lifecycle-runtime-ktx:2.8.7"
     )
+    implementation("androidx.compose.material3:material3")
+    implementation("com.google.android.material:material:1.12.0")
 
     implementation(
         "androidx.lifecycle:lifecycle-runtime-compose:2.8.7"
